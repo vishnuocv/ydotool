6aa9c186c5989cbfccbd1508e0381a2901820043
vishnuocv <vishnuocv@gmail.com>
2020-07-01 17:53:51 +0900

    changed tap

--- uinput.c
+++ uinput.c
@@ -622,13 +622,13 @@ int uinput_touch_tap_event(int x, int y)
 
 int uinput_touch_swipe_event(int startx, int starty, int endx, int endy, int duration)
 {
-	uinput_touch_tap_event (startx, starty);
-	usleep (500);
+//	uinput_touch_tap_event (startx, starty);
+//	usleep (500);
 
-//	uinput_emit(EV_KEY, BTN_TOUCH, 1);
+	uinput_emit(EV_KEY, BTN_TOUCH, 1);
         uinput_emit(EV_ABS, ABS_X, startx);
         uinput_emit(EV_ABS, ABS_Y, starty);
-	uinput_emit(EV_KEY, BTN_TOUCH, 1);
+//	uinput_emit(EV_KEY, BTN_TOUCH, 1);
         uinput_emit(EV_SYN, SYN_REPORT, 0);
 
 //      stroke_emit(EV_KEY, BTN_LEFT, 1);
