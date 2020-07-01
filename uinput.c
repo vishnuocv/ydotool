669275cae5f47dec608317fd1c47bf4fd2a03e7e
vishnuocv <vishnuocv@gmail.com>
2020-07-01 15:06:24 +0900

    changed tap

--- uinput.c
+++ uinput.c
@@ -611,16 +611,20 @@ int uinput_touch_tap_event(int x, int y)
 	uinput_emit(EV_ABS, ABS_Y, y);
 	uinput_emit(EV_SYN, SYN_REPORT, 0);
 
+	usleep (5000);
+
 //	stroke_emit(EV_KEY, BTN_LEFT, 1);
 	uinput_emit(EV_KEY, BTN_TOUCH, 1);
 	uinput_emit(EV_ABS, ABS_X, x);
 	uinput_emit(EV_ABS, ABS_Y, y);
 	uinput_emit(EV_SYN, SYN_REPORT, 0);
+	usleep (5000);
 
 	// Report KEY - RELEASE event
 //	stroke_emit(EV_KEY, BTN_LEFT, 0);
 	uinput_emit(EV_KEY, BTN_TOUCH, 0);
 	uinput_emit(EV_SYN, SYN_REPORT, 0);
+	usleep (5000);
 }
 
 int uinput_touch_swipe_event(int startx, int starty, int endx, int endy, int duration)
@@ -641,16 +645,19 @@ int uinput_touch_swipe_event(int startx, int starty, int endx, int endy, int dur
 
 	usleep(duration * 1000);
 
-        uinput_emit(EV_KEY, BTN_TOUCH, 1);
+//        uinput_emit(EV_KEY, BTN_TOUCH, 1);
         uinput_emit(EV_ABS, ABS_X, endx);
         uinput_emit(EV_ABS, ABS_Y, endy);
         uinput_emit(EV_SYN, SYN_REPORT, 0);
+	usleep (5000);
 
         // Report KEY - RELEASE event
 //      stroke_emit(EV_KEY, BTN_LEFT, 0);
-        uinput_emit(EV_KEY, BTN_TOUCH, 0);
+//        uinput_emit(EV_KEY, BTN_TOUCH, 0);
         uinput_emit(EV_ABS, ABS_X, endx);
         uinput_emit(EV_ABS, ABS_Y, endy);
+        uinput_emit(EV_KEY, BTN_TOUCH, 0);
         uinput_emit(EV_SYN, SYN_REPORT, 0);
+	usleep (5000);
 }
 
